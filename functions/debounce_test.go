df9dd8cb2af2149b468a86bb4724027f34cb9a6a
Anshuman Chhapolia <achhap.10.01@gmail.com>
2019-08-30 21:10:28 +0530

    Fixed race condition in Debounce used Non-Blocking Channels and mutex in test
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

--- functions/debounce_test.go
+++ functions/debounce_test.go
@@ -1,6 +1,7 @@
 package functions
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
@@ -8,16 +9,23 @@ import (
 func TestDebounce(t *testing.T) {
 
 	i := 0
+	mux := sync.Mutex{}
 	h := func() {
+		mux.Lock()
 		increment(&i)
+		mux.Unlock()
 	}
 	df := Debounce(h, 1000)
 	df.Call()
 	time.Sleep(3 * time.Second)
 	df.Cancel()
+	mux.Lock()
 	if i != 3 {
+		t.Log(i)
 		t.Fail()
+
 	}
+	mux.Unlock()
 }
 func increment(i *int) {
 	*i += 1
