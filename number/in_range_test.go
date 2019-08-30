3e68f68e63a6a9fba14dffab016def1f414c310a
ATechnoHazard <amolele@gmail.com>
2019-08-30 16:49:00 +0530

    number: Implement InRange function with tests
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- number/in_range_test.go
+++ number/in_range_test.go
@@ -1,1 +1,26 @@
 package number
+
+import "testing"
+
+func TestInRange(t *testing.T) {
+	if !InRange(3, 2, 4) {
+		t.Fail()
+	}
+
+	if !InRange(-3, -2, -6) {
+		defer func () {
+			recover()
+		}()
+		t.Fail()
+	}
+}
+
+func TestInRange2(t *testing.T) {
+	defer func() {
+		if recover() == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+
+	InRange(5.2, 0.0, 4)
+}
