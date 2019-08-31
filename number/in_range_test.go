be79859c898dd4b808db59f28d7345fc1bf9214f
ATechnoHazard <amolele@gmail.com>
2019-08-31 16:55:40 +0530

    number: Reduce test function complexity
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- number/in_range_test.go
+++ number/in_range_test.go
@@ -2,57 +2,7 @@ package number
 
 import "testing"
 
-func TestInRange(t *testing.T) {
-	// test for int
-	if !InRange(3, 2, 4) {
-		t.Fail()
-	}
-
-	// test for int8
-	if !InRange(int8(3), int8(2), int8(4)) {
-		t.Fail()
-	}
-
-	// test for int16
-	if !InRange(int16(3), int16(2), int16(4)) {
-		t.Fail()
-	}
-
-	// test for int32
-	if !InRange(int32(3), int32(2), int32(4)) {
-		t.Fail()
-	}
-
-	// test for int64
-	if !InRange(int64(3), int64(2), int64(4)) {
-		t.Fail()
-	}
-
-	// test for uint
-	if !InRange(uint(3), uint(2), uint(4)) {
-		t.Fail()
-	}
-
-	// test for uint8
-	if !InRange(uint8(3), uint8(2), uint8(4)) {
-		t.Fail()
-	}
-
-	// test for uint16
-	if !InRange(uint16(3), uint16(2), uint16(4)) {
-		t.Fail()
-	}
-
-	// test for uint32
-	if !InRange(uint32(3), uint32(2), uint32(4)) {
-		t.Fail()
-	}
-
-	// test for uint64
-	if !InRange(uint64(3), uint64(2), uint64(4)) {
-		t.Fail()
-	}
-
+func TestInRangeFloat(t *testing.T) {
 	// test for float32
 	if !InRange(float32(3), float32(2), float32(4)) {
 		t.Fail()
@@ -83,4 +33,58 @@ func TestInRange3(t *testing.T) {
 	}()
 
 	InRange("s", "f", "m")
+}
+
+func TestInRangeInt(t *testing.T) {
+	// test for int
+	if !InRange(3, 2, 4) {
+		t.Fail()
+	}
+
+	// test for int8
+	if !InRange(int8(3), int8(2), int8(4)) {
+		t.Fail()
+	}
+
+	// test for int16
+	if !InRange(int16(3), int16(2), int16(4)) {
+		t.Fail()
+	}
+
+	// test for int32
+	if !InRange(int32(3), int32(2), int32(4)) {
+		t.Fail()
+	}
+
+	// test for int64
+	if !InRange(int64(3), int64(2), int64(4)) {
+		t.Fail()
+	}
+}
+
+func TestInRangeUint(t *testing.T) {
+	// test for uint
+	if !InRange(uint(3), uint(2), uint(4)) {
+		t.Fail()
+	}
+
+	// test for uint8
+	if !InRange(uint8(3), uint8(2), uint8(4)) {
+		t.Fail()
+	}
+
+	// test for uint16
+	if !InRange(uint16(3), uint16(2), uint16(4)) {
+		t.Fail()
+	}
+
+	// test for uint32
+	if !InRange(uint32(3), uint32(2), uint32(4)) {
+		t.Fail()
+	}
+
+	// test for uint64
+	if !InRange(uint64(3), uint64(2), uint64(4)) {
+		t.Fail()
+	}
 }
