7971b71cb7819d7fd15a3a7c96ee416b4f0bb5c2
Anshuman Chhapolia <achhap.10.01@gmail.com>
2019-08-31 10:13:30 +0530

    Implement delay functionality Dropped Once because doesn't seem to be needed.
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

---
+++ functions/delay_test.go
@@ -0,0 +1,30 @@
+package functions
+
+import (
+	"testing"
+	"time"
+)
+
+// TestDelay we can use Non-Blocking channels
+// If after a wait time there is no buffer to read from
+// the channel then the test fails
+func TestDelay(t *testing.T) {
+	ch := make(chan string)
+
+	f := func() {
+		ch <- "hi"
+	}
+	Delay(f, 3000)
+	time.Sleep(3 * time.Second)
+	select {
+	case h := <-ch:
+		{
+			if h != "hi" {
+				t.Logf("Recieved :%s", h)
+				t.Fail()
+			}
+		}
+	default:
+		t.Fail()
+	}
+}
